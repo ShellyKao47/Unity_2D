15e82092dc569b625c1cf0d2957606e5a5a208a5
ShellyKao47 <75553497+ShellyKao47@users.noreply.github.com>
2021-01-03 13:02:35 +0800

    開始設定

---
+++ 2D_Warrior/Assets/Script/API.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class API : MonoBehaviour
+{
+    public Transform TraA;
+    public Transform Tester;
+    public SpriteRenderer spr;
+    public Camera cam;
+    public GameObject gam;
+
+    private void Start()
+    {
+        print("座標：" + TraA.position);
+
+       // Tester.position = new Vector3(2, 0, 0);
+
+        cam.backgroundColor = new Color(0.3f, 0.5f, 0.5f);
+        //練習
+        spr.flipX = true;
+        gam.layer = 5;
+        print("顏色：" + spr.color);
+        print("圖層：" + gam.layer);
+
+    }
+    private void Update()
+    {
+        Tester.Rotate(0, 0.5f, 0);
+        Tester.Translate(2, 0, 0, Space.World);
+    }
+}
---
+++ 2D_Warrior/Assets/Script/SceneControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
