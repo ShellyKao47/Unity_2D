e098ed72680a6bcfb9a0e5a24ac65793b1982cd7
ShellyKao47 <75553497+ShellyKao47@users.noreply.github.com>
2021-01-31 16:09:32 +0800

    更新魔王觸發

---
+++ 2D_Warrior/Assets/Script/Bossap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Bossap : MonoBehaviour
+{
+    [Header("魔王血條")]
+    public GameObject bl;
+    [Header("魔王")]
+    public GameObject bossap;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name=="人物")
+        {
+            bl.SetActive(true);
+            bossap.SetActive(true);
+        }
+    }
+}
